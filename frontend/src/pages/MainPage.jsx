04855344249b5f8b688194997e20097f8459d59b
Starman-in-the-sky <ms.practice@yandex.ru>
2023-10-14 09:17:16 +0300

    Minor changes ver.2

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,6 +1,9 @@
 import {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
+import {YandexMap} from "../components/YandexMap";
 
 export const MainPage = () => {
-    <div className="w-1/2 h-1/2 bg-purple-300">IMHO</div>
+    return(
+        <YandexMap/>
+    )
 }
