a1489b36567faa3430590ed772f7ec51527a8b35
Starman-in-the-sky <ms.practice@yandex.ru>
2023-10-14 10:55:49 +0300

    Actually 2 maps

--- frontend/src/components/YandexMap.jsx
+++ frontend/src/components/YandexMap.jsx
@@ -1,20 +1,30 @@
-import {
-    createBrowserRouter, Link, NavLink, Outlet,
-    RouterProvider, useNavigate,
-} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, { useEffect, useRef } from "react";
 
-export const YandexMap = () => {
-    return(
-        <div>
-            <head>
-                <script src="https://api-maps.yandex.ru/2.1/?apikey=<5941ca0f-88a5-462a-b68b-049e98bd248c>&lang=ru_RU" type="text/javascript">
-                </script>
-            </head>
-            <body>
-                <div id="map" className= "w-[1176px] h-[644px]"></div>
-                <script src="../scripts/LoadingYandexMap.js" type="text/javascript"></script>
-            </body>
-        </div>
-    );
-}
+const YandexMap = () => {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        // Загрузка API Яндекс.Карт
+        const script = document.createElement("script");
+        script.src = "https://api-maps.yandex.ru/2.1/?apikey=5941ca0f-88a5-462a-b68b-049e98bd248c&lang=ru_RU";
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            // Инициализация карты внутри функции ymaps.ready()
+            window.ymaps.ready(() => {
+                const map = new window.ymaps.Map(mapRef.current, {
+                    center: [55.75, 37.57],
+                    zoom: 10,
+                });
+
+                const placemark = new window.ymaps.Placemark([55.75, 37.57], {}, {});
+                map.geoObjects.add(placemark);
+            });
+        };
+    }, []);
+
+    return <div ref={mapRef} style={{ width: "1176px", height: "640px" }} />;
+};
+
+export default YandexMap;
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
-import {YandexMap} from "../components/YandexMap";
+import YandexMap from "../components/YandexMap";
 
 export const MainPage = () => {
     return(
