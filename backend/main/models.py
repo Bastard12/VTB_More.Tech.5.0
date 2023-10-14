5b19e26880bde61cc489b2a9ea8675fbaace3764
hacha <hachatryan.artem.2002@gmail.com>
2023-10-14 03:18:47 +0300

    Added files

--- backend/main/models.py
+++ backend/main/models.py
@@ -29,3 +29,35 @@ class atms(models.Model):
 
     def __str__(self):
         return self.address
+
+
+class Offices(models.Model):
+    salePointName = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    status = models.CharField(max_length=15)
+    rko = models.CharField(max_length=15)
+    officeType = models.CharField(max_length=15)
+    salePointFormat = models.CharField(max_length=15)
+    suoAvailability = models.CharField(max_length=1)
+    hasRamp = models.CharField(max_length=1)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    metroStation = models.CharField(max_length=255, null=True, blank=True)
+    distance = models.IntegerField()
+    kep = models.BooleanField()
+    myBranch = models.BooleanField()
+
+    def __str__(self):
+        return self.salePointName
+
+
+class OpenHours(models.Model):
+    office = models.ForeignKey(Offices, on_delete=models.CASCADE)
+    days = models.CharField(max_length=10)
+    hours = models.CharField(max_length=20)
+
+
+class OpenHoursIndividual(models.Model):
+    office = models.ForeignKey(Offices, on_delete=models.CASCADE)
+    days = models.CharField(max_length=10)
+    hours = models.CharField(max_length=20)
