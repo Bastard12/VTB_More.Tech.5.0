8d9038567e5e73a9008bfb6353859da6f9041609
Starman-in-the-sky <ms.practice@yandex.ru>
2023-10-14 11:08:24 +0300

    Actually working map

--- frontend/src/components/YandexMap.jsx
+++ frontend/src/components/YandexMap.jsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const YandexMap = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
     const mapRef = useRef(null);
 
     useEffect(() => {
         // Загрузка API Яндекс.Карт
         const script = document.createElement("script");
-        script.src = "https://api-maps.yandex.ru/2.1/?apikey=5941ca0f-88a5-462a-b68b-049e98bd248c&lang=ru_RU";
+        script.src =
+            "https://api-maps.yandex.ru/2.1/?apikey=5941ca0f-88a5-462a-b68b-049e98bd248c&lang=ru_RU";
         script.async = true;
         document.body.appendChild(script);
 
         script.onload = () => {
-            // Инициализация карты внутри функции ymaps.ready()
+            // Установка значения isLoaded в true при загрузке API
+            setIsLoaded(true);
+        };
+    }, []);
+
+    useEffect(() => {
+        // Отрисовка карты только после загрузки API
+        if (isLoaded) {
             window.ymaps.ready(() => {
                 const map = new window.ymaps.Map(mapRef.current, {
                     center: [55.75, 37.57],
                     zoom: 10,
                 });
-
-                const placemark = new window.ymaps.Placemark([55.75, 37.57], {}, {});
-                map.geoObjects.add(placemark);
             });
-        };
-    }, []);
+        }
+    }, [isLoaded]);
 
-    return <div ref={mapRef} style={{ width: "1176px", height: "640px" }} />;
+    return (
+        <div ref={mapRef} style={{ width: "1176px", height: "640px" }} />
+    );
 };
 
 export default YandexMap;
